9cb96092ca2e7e6df6f8e2da52d1f41475c7262c
thebennies <benny.sudaryanto@gmail.com>
2026-04-24 12:10:51 +0700

    ⚙️ enable PWA dev options in Vite config

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -7,6 +7,9 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
       includeAssets: ["favicon.png", "hero.webp"],
       manifest: {
         name: "KolektaPOS",
